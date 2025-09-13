55a2689be655bf8b1b8c822f11b8b2f34b480b5d
Aleksey <a_h.79@mail.ru>
2025-09-13 21:59:06 +0700

    - добавлен вывод графика импеданса от частоты - оптимизирован скрипт - в модели отключен генератор меандра

--- RandlesMain.m
+++ RandlesMain.m
@@ -9,18 +9,20 @@ simIn = Simulink.SimulationInput(model);
 
 % get_param('Randles/Sine Wave','dialogparameters') % получает все параметры компонента Step
 
-freq_start = 3;
+freq_start = 1;
 freq_stop  = 10000;
-freq_step  = 200;
+freq_step  = 10;
 
-arr_size = (freq_stop/freq_step);
-result = zeros(1, arr_size);
-frequencies = zeros(1, arr_size);
-cnt = 1;
+arr_size = (freq_stop/freq_step); % рассчёт размера массива результатов измерения
+result = zeros(1, arr_size); % массив импедансов для ряда частот
+frequencies = zeros(1, arr_size); % массив частот для измерения результатов
+cnt = 1; % счётчик массива результата измерений импеданса
 
-if freq_start == 0
+if freq_start == 0 % защита от ввода нулевой частоты
     freq_start = 1;
 end
+
+% Цикл рассчёта импеданса
 for freq = freq_start : freq_step : freq_stop
 
 % freq = 1000; % частота в Гц
@@ -41,6 +43,7 @@ disp('Simulation run...');
 out = sim(simIn);
 disp('Simulation sucsess.');
 
+% Транспонирование матриц для удобства
 Y = transpose(out.simout.Data);
 t = transpose(out.simout.Time);
 
@@ -55,8 +58,8 @@ L = t_len;              % общее кол-во захваченных сэмп
 x_diff = diff(t);       % разница времен м.у сэмплами
 Ts = mean(x_diff);      % приод дискретизации
 Fs = 1/Ts;              % частота дискретизации
-Yabs = abs(Y_fft);       % массив модулей fft
-freq_ax = Fs/L*(0:L-1); % частоты по оси x.
+Yabs = abs(Y_fft);      % массив модулей fft
+freq_ax = Fs/L*(0:L-1); % массив частот по оси x.
 
 % Поиск амплитуды сигнала
 for i = 1:1:L
@@ -72,6 +75,7 @@ end % for freq = 1:1:L
 % Построение графиков
 tiledlayout(2,1);
 
+%  Амплитуда/частота
 ax1 = nexttile;
 plot(ax1, freq_ax, Yabs, "LineWidth", 2);
 title(ax1, 'FFT');
@@ -81,6 +85,7 @@ grid on
 
 hold on
 
+% Исходный сигнал
 ax0 = nexttile;
 plot(ax0, t, Y);
 title(ax0, 'Original signal');
@@ -88,16 +93,31 @@ xlabel('time');
 ylabel('Ampl');
 
 hold off
+pause(0.1);
 % close_system(model); % закрывает симулинк
 end % for freq = 100:1000:100
 
 figure;
 ax_res = nexttile;
-% plot(ax_res, frequencies, imag(result), "LineWidth", 2);
+% График найквиста
 plot(ax_res, real(result), -imag(result), "LineWidth", 2);
-title(ax_res, 'Result');
+title(ax_res, 'Impedance Nyquist');
 xlabel(ax_res, 'real(Z)');
-ylabel(ax_res, 'imag(Z)');
+ylabel(ax_res, '-imag(Z)');
+grid on
+
+figure;
+ax_res1 = nexttile;
+% Логарифмический график амплитуда/частота
+hold on
+loglog(ax_res1, frequencies, abs(result), "LineWidth", 2);
+loglog(ax_res1, frequencies, real(result), "LineWidth", 2);
+loglog(ax_res1, frequencies, imag(result), "LineWidth", 2);
+hold off
+title(ax_res1, 'Impedance log axies');
+xlabel(ax_res1, 'f, Hz');
+ylabel(ax_res1, 'Amplitude');
+legend(ax_res1, 'abs', 'real', 'imag');
 grid on
 
 
